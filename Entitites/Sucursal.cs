a27a17a438163bdebf3ddf7accd46fff553e96aa
Hezroiner <146049408+Hezroiner@users.noreply.github.com>
2024-05-16 23:50:03 -0600

    Add files via upload
    entities

---
+++ Entitites/Sucursal.cs
@@ -0,0 +1,20 @@
+﻿namespace Proyecto_II.Entities
+{
+    public class Sucursal
+    {
+        public int Id { get; set; }
+        public DateTime FechaHora { get; set; }
+        public string Lugar { get; set; }
+        public string Status { get; set; }
+
+
+        public int PacienteId { get; set; }
+        public Paciente Paciente { get; set; }
+        public int TipoCitaId { get; set; }
+        public TipoCita TipoCita { get; set; }
+        public int SucursalId { get; set; }
+        public Sucursal Sucursales { get; set; }
+        public List<Cita> Citas { get; set; }
+
+    }
+}
---
+++ Entitites/TipoCita.cs
@@ -0,0 +1,9 @@
+﻿namespace Proyecto_II.Entities
+{
+    public class TipoCita
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<Cita> Citas { get; set; }
+    }
+}
