0729bebc15ba183b92ec499fc17d72580f6b28f3
Hezron <hezroncr@gmail.com>
2024-05-23 20:50:57 -0600

    ? en algunas entidades para poder pasar datos

--- Entitites/User.cs
+++ Entitites/User.cs
@@ -9,8 +9,8 @@
         public string Password { get; set; }
 
         public int RoleId { get; set; }
-        public Role Role { get; set; }
-        public List<Cita> Citas { get; set; }
+        public Role? Role { get; set; }
+        public List<Cita>? Citas { get; set; }
 
     }
 }
