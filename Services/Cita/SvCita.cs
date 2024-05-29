dbdb82539418f170f0cd17c96816131bd904436f
Hezron <hezroncr@gmail.com>
2024-05-29 01:55:03 -0600

    Envio de Email al correo destinado

--- Services/Cita/SvCita.cs
+++ Services/Cita/SvCita.cs
@@ -1,20 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Proyecto_II.Entities;
 using Services;
 using Services.MyDbContext;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Mail;
+using System.Net;
 
 namespace Proyecto_II.Services
 {
     public class SvCita : ICita
     {
         private readonly MyContext _myContext;
+        private readonly IConfiguration _configuration;
 
-        public SvCita(MyContext myContext)
+        public SvCita(MyContext myContext, IConfiguration configuration)
         {
             _myContext = myContext;
+            _configuration = configuration;
         }
 
         public Cita AddCita(Cita cita)
@@ -29,7 +31,49 @@ namespace Proyecto_II.Services
             _myContext.Citas.Add(cita);
             _myContext.SaveChanges();
 
+
+            SendEmail(cita);
             return cita;
+        }
+
+        private void SendEmail(Cita cita)
+        {
+            var user = _myContext.Users.FirstOrDefault(u => u.UserId == cita.UserId);
+            if (user == null)
+            {
+                // Manejar el caso cuando el usuario no está encontrado
+                return;
+            }
+
+            // Cargar el tipo de cita y la sucursal desde la base de datos
+            var tipoCita = _myContext.TiposCita.FirstOrDefault(tc => tc.TipoCitaId == cita.TipoCitaId);
+            var sucursal = _myContext.Sucursales.FirstOrDefault(s => s.SucursalId == cita.SucursalId);
+
+            var emailFrom = _configuration["EmailSettings:From"];
+            var smtpServer = _configuration["EmailSettings:SmtpServer"];
+            var smtpPort = int.Parse(_configuration["EmailSettings:Port"]);
+            var smtpUsername = _configuration["EmailSettings:Username"];
+            var smtpPassword = _configuration["EmailSettings:Password"];
+            var emailTo = user.Email;
+            var subject = "Nueva cita creada";
+
+            // Obtener el nombre del tipo de cita y la sucursal
+            var tipoCitaNombre = tipoCita != null ? tipoCita.Nombre : "No especificado";
+            var sucursalNombre = sucursal != null ? sucursal.Nombre : "No especificado";
+
+            var body = $"Se ha creado una nueva cita para usted. Detalles: Fecha: {cita.FechaHora}, Tipo: {tipoCitaNombre}, Sucursal: {sucursalNombre}.";
+
+            using (var message = new MailMessage(emailFrom, emailTo, subject, body))
+            {
+                using (var client = new SmtpClient(smtpServer, smtpPort))
+                {
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                    client.EnableSsl = true;
+
+                    client.Send(message);
+                }
+            }
         }
 
         public void Delete(int id)
